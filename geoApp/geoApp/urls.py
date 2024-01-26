a964b76421377123ceeecc6b4f033aa8ea961bab
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-01-26 15:48:01 +0530

    admin can now publish the shapefile to the db

--- geoApp/geoApp/urls.py
+++ geoApp/geoApp/urls.py
@@ -1,7 +1,7 @@
 """geoApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shp.views import index
+# from note.views import note
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    # path('note/', note, name='note')
 ]
