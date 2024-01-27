2d4a28401ca0f6c63b733af2a0e8a70277702ba7
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-01-27 18:32:46 +0530

    updated the ui for the pin and zoom layer

--- geoApp/geoApp/settings.py
+++ geoApp/geoApp/settings.py
@@ -92,7 +92,8 @@ DATABASES = {
         'NAME': 'geoapp',
         'USER': 'postgres',
         'PASSWORD': 'muthu12345',
-        'HOST': 'database-1.cla06cywkakj.ap-south-1.rds.amazonaws.com',
+        # 'HOST': 'database-1.cla06cywkakj.ap-south-1.rds.amazonaws.com',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }        
@@ -140,12 +141,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 
-AWS_ACCESS_KEY_ID = 'AKIAYS2NV5DW6WVZIGBC '
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'geoproject1'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'ap-south-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'        
+# AWS_ACCESS_KEY_ID = 'AKIAYS2NV5DW6WVZIGBC '
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'geoproject1'
+# AWS_S3_SIGNATURE_NAME = 's3v4',
+# AWS_S3_REGION_NAME = 'ap-south-1'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL =  None
+# AWS_S3_VERITY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'        
--- geoApp/shp/models.py
+++ geoApp/shp/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 import datetime
-import tempfile
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from django.core.files.storage import default_storage
 import geopandas as gpd
-import boto3
 import os
 import glob
 import zipfile
@@ -18,10 +15,10 @@ from pg.pg import Pg
 ####################################################################################
 # initializing the library
 db = Pg(dbname='geoapp', user='postgres',
-        password='muthu12345', host='database-1.cla06cywkakj.ap-south-1.rds.amazonaws.com', port='5432')
+        password='muthu12345', host='localhost', port='5432')
 geo = Geoserver('http://127.0.0.1:8080/geoserver', username='admin', password='Skyblue@1002')
 # Database connection string (postgresql://${database_user}:${databse_password}@${database_host}:${database_port}/${database_name}
-conn_str = 'postgresql://postgres:muthu12345@database-1.cla06cywkakj.ap-south-1.rds.amazonaws.com:5432/geoapp'
+conn_str = 'postgresql://postgres:muthu12345@localhost:5432/geoapp'
 
 
 ######################################################################################
@@ -42,73 +39,52 @@ class Shp(models.Model):
 # #########################################################################################
 @receiver(post_save, sender=Shp)
 def publish_data(sender, instance, created, **kwargs):
-    if not created:
-        return
+    file = instance.file.path
+    file_format = os.path.basename(file).split('.')[-1]
+    file_name = os.path.basename(file).split('.')[0]
+    file_path = os.path.dirname(file)
+    name = instance.name
 
-    # AWS S3 client setup
-    s3_client = boto3.client(
-    's3',
-    aws_access_key_id='AKIAYS2NV5DW6WVZIGBC',
-    aws_secret_access_key='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',
-    region_name='ap-south-1'
-)
+    # extract zipfile
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall(file_path)
 
-    bucket_name = instance.file.storage.bucket.name
-    file_key = instance.file.name
-    print("hello")
-    # Use a temporary directory to handle the file
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        local_file_path = os.path.join(tmp_dir, os.path.basename(file_key))
+    os.remove(file)  # remove zip file
 
-        # Download the file from S3
-        s3_client.download_file(bucket_name, file_key, local_file_path)
-        print("hello1")
-        # Now proceed with your original logic
-        file_format = os.path.basename(local_file_path).split('.')[-1]
-        file_name = os.path.basename(local_file_path).split('.')[0]
-        file_path = tmp_dir
-        name = instance.name
-        print("hello3")
-        # Extract zipfile
-        with zipfile.ZipFile(local_file_path, 'r') as zip_ref:
-            zip_ref.extractall(file_path)
-            print(f"Extracted files to {file_path}")
-        extracted_files = os.listdir(file_path)
-        print("Extracted files:", extracted_files)
+    shp = glob.glob(r'{}/**/*.shp'.format(file_path),
+                    recursive=True)  # to get shp
+    try:
+        req_shp = shp[0]
+        gdf = gpd.read_file(req_shp)  # make geodataframe
+        engine = create_engine(conn_str)
+        gdf.to_postgis(
+            con=engine,
+            schema='data',
+            name=name,
+            if_exists="replace")
 
+        for s in shp:
+            os.remove(s)
 
-        # Find .shp file in the extracted files
-        shp = glob.glob(f'{file_path}/**/*.shp', recursive=True)
-        print("Shape files found:", shp)
+    except Exception as e:
+        for s in shp:
+            os.remove(s)
 
-        try:
-            req_shp = shp[0]
-            gdf = gpd.read_file(req_shp)  # Make GeoDataFrame
-            engine = create_engine(conn_str)
-            print("Attempting to write GeoDataFrame to PostGIS")
-            gdf.to_postgis(con=engine, schema='data', name=name, if_exists="replace")
-            print("Write operation successful")
+        instance.delete()
+        print("There is problem during shp upload: ", e)
 
+    # publish shp to the geoserver using geoserver-rest
+    geo.create_featurestore(store_name='geoApp', workspace='geoapp', db='geoapp',
+                            host='localhost', pg_user='postgres', pg_password='muthu12345', schema='data')
+    
+    
+    geo.publish_featurestore(
+        workspace='geoapp', store_name='geoApp', pg_table=name)
 
-        except Exception as e:
-            print("There is a problem during .shp upload: ", e)
-            instance.delete()
-            return
+    geo.create_outline_featurestyle('geoApp_shp', workspace='geoapp')
 
-        # Publish .shp to the geoserver using geoserver-rest
-        geo.create_featurestore(store_name='geoApp', workspace='geoapp', db='geoapp',
-                                host='database-1.cla06cywkakj.ap-south-1.rds.amazonaws.com', 
-                                pg_user='postgres', pg_password='muthu12345', schema='data')
-        print("create operation successful")
-        geo.publish_featurestore(workspace='geoapp', store_name='geoApp', pg_table=name)
-        print("Publish operation successful")
-        geo.create_outline_featurestyle('geoApp_shp', workspace='geoapp')
-        geo.publish_style(layer_name=name, style_name='geoApp_shp', workspace='geoapp')
-
-        # # Clean up temporary files
-        # for file in os.listdir(file_path):
-        #     os.remove(os.path.join(file_path, file))
-
+    geo.publish_style(
+        layer_name=name, style_name='geoApp_shp', workspace='geoapp')
 
 #########################################################################################
 # Django post delete signal
